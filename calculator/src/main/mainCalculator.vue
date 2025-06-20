507d793ee749269dcdaaaabd401f57fa90a1ec4f
Mateus Urtassum <mateusurtassum@gmail.com>
2025-06-20 15:37:58 -0300

    work with button component

--- calculator/src/main/mainCalculator.vue
+++ calculator/src/main/mainCalculator.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="calculator">
-        <appButton />
         <appDisplay />
+        <appButton label="AC" triple/>
+        <appButton label="/" operation/>
+        <appButton label="7"/>
+        <appButton label="8"/>
+        <appButton label="9"/>
+        <appButton label="*" operation/>
+        <appButton label="4"/>
+        <appButton label="5"/>
+        <appButton label="6"/>
+        <appButton label="-" operation/>
+        <appButton label="1"/>
+        <appButton label="2"/>
+        <appButton label="3"/>
+        <appButton label="+" operation/>
+        <appButton label="0" fouble/>
+        <appButton label="."/>
+        <appButton label="=" operation/>
     </div>
 </template>
 
