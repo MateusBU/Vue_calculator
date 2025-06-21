3c27fecf2d6beacd484eefcf7d200fb0ffa75f29
Mateus Urtassum <mateusurtassum@gmail.com>
2025-06-21 13:59:20 -0300

    add methods and display

--- calculator/src/main/mainCalculator.vue
+++ calculator/src/main/mainCalculator.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="calculator">
-        <appDisplay />
-        <appButton label="AC" triple/>
-        <appButton label="/" operation/>
-        <appButton label="7"/>
-        <appButton label="8"/>
-        <appButton label="9"/>
-        <appButton label="*" operation/>
-        <appButton label="4"/>
-        <appButton label="5"/>
-        <appButton label="6"/>
-        <appButton label="-" operation/>
-        <appButton label="1"/>
-        <appButton label="2"/>
-        <appButton label="3"/>
-        <appButton label="+" operation/>
-        <appButton label="0" fouble/>
-        <appButton label="."/>
-        <appButton label="=" operation/>
+        <appDisplay value="100"/>
+        <appButton label="AC" triple @onCalcButtonClick="clearMemory"/>
+        <appButton label="/" operation @onCalcButtonClick="setOperation"/>
+        <appButton label="7" @onCalcButtonClick="addDigit"/>
+        <appButton label="8" @onCalcButtonClick="addDigit"/>
+        <appButton label="9" @onCalcButtonClick="addDigit"/>
+        <appButton label="*" operation @onCalcButtonClick="setOperation"/>
+        <appButton label="4" @onCalcButtonClick="addDigit"/>
+        <appButton label="5" @onCalcButtonClick="addDigit"/>
+        <appButton label="6" @onCalcButtonClick="addDigit"/>
+        <appButton label="-" operation @onCalcButtonClick="setOperation"/>
+        <appButton label="1" @onCalcButtonClick="addDigit"/>
+        <appButton label="2" @onCalcButtonClick="addDigit"/>
+        <appButton label="3" @onCalcButtonClick="addDigit"/>
+        <appButton label="+" operation @onCalcButtonClick="setOperation"/>
+        <appButton label="0" double @onCalcButtonClick="addDigit"/>
+        <appButton label="." @onCalcButtonClick="addDigit"/>
+        <appButton label="=" operation @onCalcButtonClick="setOperation"/>
     </div>
 </template>
 
@@ -26,7 +26,18 @@ import appDisplay from '@/components/compDisplay.vue';
 import appButton from '@/components/compButton.vue';
 
 export default{
-    components: {appButton, appDisplay}
+    components: {appButton, appDisplay},
+    methods:{
+        clearMemory(){
+            
+        },
+        setOperation(operation){
+            
+        },
+        addDigit(n){
+
+        }
+    }
 }
 </script>
 
